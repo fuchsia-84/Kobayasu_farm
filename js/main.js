f5d62ac7f7ebbfc6d6056b835a98565c4ea30a5b
fuchsia-84 <mikitech9084@gmail.com>
2021-12-15 20:21:31 +0900

    curve shop button text.

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@ $(function() {
     $toggle_btn = $('.toggle_btn');
     $header = $('#header');
     $buttons = $('.buttons');
+    $shop_btn_text = $('.shop_btn a div');
     $header_global_nav = $('#header .global_nav');
     var header_height = $header.outerHeight();
     var window_height = $(window).height();
     var switch_adjust = 400;
     var shop_adjust = 600;
     var on_footer_adjust = 2000;
+
+    /* letter_span : テキストを1字ずつ区切ってspanで囲む*/
+    $.fn.letter_span = function() {
+        $(this).each(function() {
+            var text = $.trim(this.textContent),
+                i = 0,
+                html = "";
+            
+            text.split("").forEach(function(v) {
+                html += "<span id=" + "'char_" + i + "'>" + v + "</span>";
+                i++;
+            });
+            this.innerHTML = html;
+        });
+    };
+    $shop_btn_text.letter_span();
 
     /* scroll over header height, change header style */
     $(window).on('scroll', function() {
@@ -36,7 +53,7 @@ $(function() {
         }
     });
 
-    /* scroll over on footer, change page_top button style*/
+    /* scroll over on footer, change page_top button style */
     $(window).on('scroll', function() {
         if (on_footer_adjust < $(window).scrollTop()) {
             $buttons.addClass('on_footer');
@@ -44,7 +61,6 @@ $(function() {
             $buttons.removeClass('on_footer');
         }
     });
-
 
     /* toggle button clicked, show global navigation
     $toggle_btn.click(function() {
